6147b2dcc10fea3fb515bc1a42290ef6c7730afa
Wayne Hatter <wayne@yourwpmadesimple.com>
2022-01-03 01:58:02 -0500

    first commit

---
+++ request.js
@@ -0,0 +1,29 @@
+const https = require("https");
+const fs = require("fs");
+
+const options = {
+  hostname: "en.wikipedia.org",
+  port: 443,
+  path: "/wiki/Snoopy",
+  method: "GET",
+};
+
+const request = https.request(options, (res) => {
+  let responseBody = "";
+  res.setEncoding("UTF-8");
+
+  res.on("data", (data) => {
+    console.log("----chunk----", data.length);
+    responseBody += data;
+  });
+
+  res.on("end", () => {
+    fs.writeFile("./snoopy.html", responseBody, (err) => {
+      if (err) {
+        throw err;
+      }
+      console.log("file downloaded");
+    });
+  });
+});
+request.end();
