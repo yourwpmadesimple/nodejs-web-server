88691a10a1ad223b61785e213315ad8ed0528304
Wayne Hatter <wayne@yourwpmadesimple.com>
2022-01-09 07:03:50 -0500

    initial commit

--- countdown.js
+++ countdown.js
@@ -1,3 +1,15 @@
+const parseArgs = require("minimist");
+
+const { time } = parseArgs(process.argv);
+
+if (!time) {
+  throw new Error("--time is required");
+}
+
+if (!parseInt(time)) {
+  throw new Error("--time must be a number");
+}
+console.log(time);
 //
 // Collect the time from argv
 //
