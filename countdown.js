43d29c7896d6e5a64b59e686bb5dafc7af462b1e
Wayne Hatter <wayne@yourwpmadesimple.com>
2022-01-09 07:54:43 -0500

    added single-line-log via npm

--- countdown.js
+++ countdown.js
@@ -1,5 +1,5 @@
 const parseArgs = require("minimist");
-
+const { stdout: log } = require("single-line-log");
 const { time } = parseArgs(process.argv);
 
 if (!time) {
@@ -9,7 +9,20 @@ if (!time) {
 if (!parseInt(time)) {
   throw new Error("--time must be a number");
 }
-console.log(time);
+
+const count = parseInt(time);
+let message = "";
+
+for (let i = 0; i < count; i++) {
+  message += "*";
+}
+
+log(message);
+
+setTimeout(() => {
+  log("overwrites line");
+}, 2000);
+
 //
 // Collect the time from argv
 //
