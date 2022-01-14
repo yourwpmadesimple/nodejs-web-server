bcc09d454fb57f501cce18add6a5763936555492
Wayne Hatter <wayne@yourwpmadesimple.com>
2022-01-14 15:08:32 -0700

    added body-parser

--- server.js
+++ server.js
@@ -1,11 +1,36 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+
 let skiTerms = require("./skiTerms.json");
+
 const app = express();
+
+const save = () => {
+  fs.writeFile(
+    "./skiTerms.json",
+    JSON.stringify(skiTerms, null, 2),
+    (error) => {
+      if (error) {
+        throw error;
+      }
+    }
+  );
+};
 
 app.use(express.static("./client"));
 
 app.get("/dictionary", (req, res) => {
   res.json(skiTerms);
+});
+
+app.post("/dictionary", bodyParser.json(), (req, res) => {
+  skiTerms.push(req.body);
+  save();
+  res.json({
+    status: "success",
+    term: req.body,
+  });
 });
 
 app.listen(3000, () => {
